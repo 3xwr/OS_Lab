9eca375e7d5302b84d570fdfe7e305edddaf0f64
dx <medic8070@gmail.com>
2020-02-21 16:40:48 +0300

    local variables (struct)

--- lab2/lab2.1/lab2.cpp
+++ lab2/lab2.1/lab2.cpp
@@ -4,23 +4,26 @@
 #include <unistd.h>
 #include <semaphore.h>
 
-bool thr1_cancel = false;
-bool thr2_cancel = false;
+struct thread_args
+{
+    bool thread_cancel_flag = false;
+    sem_t* arg_semaphore;
 
-sem_t semaphore;
+};
 
 static void* thread1_start(void* args)
 {
+    thread_args* t1 = (thread_args* ) args;
     std::cout << "THREAD 1 STARTING\n" << std::flush;
-    while(!thr1_cancel)
+    while(!t1->thread_cancel_flag)
     {
-        sem_wait(&semaphore);
-        for(int i = 0; i<10; i++)
+        sem_wait(t1->arg_semaphore);
+        for(int i = 0; i<5; i++)
         {
             std::cout << '1' << std::flush;
-            sleep(0.1);
+            sleep(1);
         }
-        sem_post(&semaphore);
+        sem_post(t1->arg_semaphore);
         sleep(1);
     }   
     std::cout << "THREAD 1 EXITING\n" << std::flush;
@@ -28,16 +31,17 @@ static void* thread1_start(void* args)
 
 static void* thread2_start(void* args)
 {
+    thread_args* t2 = (thread_args* ) args;
     std::cout << "THREAD 2 STARTING\n" << std::flush;
-    while(!thr2_cancel)
+    while(!t2->thread_cancel_flag)
     {
-        sem_wait(&semaphore);
-        for(int i = 0; i<10; i++)
+        sem_wait(t2->arg_semaphore);
+        for(int i = 0; i<5; i++)
         {
             std::cout << '2' << std::flush;
-            sleep(0.1);
+            sleep(1);
         }
-        sem_post(&semaphore);
+        sem_post(t2->arg_semaphore);
         sleep(1);
     }   
     std::cout << "THREAD 2 EXITING\n" << std::flush;
@@ -45,15 +49,20 @@ static void* thread2_start(void* args)
 
 int main()
 {
+    sem_t semaphore;
+    thread_args t1_args;
+    thread_args t2_args;
+    t1_args.arg_semaphore = &semaphore;
+    t2_args.arg_semaphore = &semaphore;
     std::cout << "MAIN STARTING\n" << std::flush;
     pthread_t thread1;
     pthread_t thread2;
     sem_init(&semaphore,0,1);
-    pthread_create(&thread1, NULL,thread1_start, NULL);
-    pthread_create(&thread2, NULL,thread2_start, NULL);
+    pthread_create(&thread1, NULL,thread1_start, &t1_args);
+    pthread_create(&thread2, NULL,thread2_start, &t2_args);
     getchar();
-    thr1_cancel = true;
-    thr2_cancel = true;
+    t1_args.thread_cancel_flag = true;
+    t2_args.thread_cancel_flag = true;
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
     sem_destroy(&semaphore);
