6332a7609c6481372525a3f99097e3e32356593b
dx <medic8070@gmail.com>
2020-04-22 18:16:41 +0300

    lab 7 final

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -6,12 +6,19 @@
 #include <unistd.h>
 #include <iostream>
 #include <string.h>
+#include <signal.h>
 #define BUFFER_SIZE 7
 
 bool thread_cancel = false;
 int fd;
 
-static void* thread_func(void* arg)
+struct arg_struct
+{
+    pthread_t t_thread;
+    pthread_t o_thread;
+};
+
+static void* transfer_thread_func(void* arg)
 {
     char buf[BUFFER_SIZE];
     while(!thread_cancel)
@@ -22,15 +29,41 @@ static void* thread_func(void* arg)
     }
 }
 
+static void* open_thread_func(void* arg)
+{
+    struct arg_struct *args = (struct arg_struct *) arg;
+    while(!thread_cancel)
+    {
+        fd = open("/tmp/my_named_pipe", O_WRONLY|O_NONBLOCK);
+        if(fd == -1)
+        {
+            std::cout << "error opening the file" << std::endl;
+            sleep(2);
+        }
+        else
+        {
+            pthread_create(&args->t_thread, NULL, transfer_thread_func, NULL);
+            //pthread_join(args->o_thread, NULL); 
+            std::cout << "file opened successfully" << std::endl;
+            pthread_exit(arg);
+        }
+    }
+}
+
 int main()
 {
-    pthread_t thread;
+    signal(SIGPIPE, SIG_IGN);
+    struct arg_struct args;
+    pthread_t open_thread;
+    pthread_t transfer_thread;
+    args.t_thread = transfer_thread;
+    args.o_thread = open_thread;
     mkfifo("/tmp/my_named_pipe", 0644);
-    fd = open("/tmp/my_named_pipe", O_WRONLY|O_NONBLOCK);
-    pthread_create(&thread, NULL, thread_func, NULL);
+    pthread_create(&open_thread, NULL, open_thread_func, (void*)&args);
     getchar();
     thread_cancel = true;
-    pthread_join(thread, NULL); 
+    pthread_join(open_thread, NULL); 
+    pthread_join(transfer_thread, NULL); 
     close(fd);
     unlink("/tmp/my_named_pipe");
     return 0;
